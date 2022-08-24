afd3fb7c7d7232b24eb4a17ed4a21ee318b4696b
Mueem-Nahid <mueem51@gmail.com>
2022-08-24 23:31:14 +0600

    init, react router dom initialized and more

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <h1>Welcome to React Router!</h1>
+         <Routes>
+            <Route path="/login" element={<Login />} exact />
+            <Route path="/profile" element={<Profile />} exact />
+            <Route path="/" element={<Home />} exact />
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
+import "./styles/icons/icons.css";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <React.StrictMode>
+      <Router>
+         <App />
+      </Router>
+   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
