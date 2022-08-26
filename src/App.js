a2e68962c921eb3d8baf0511d233600ce7067398
Mueem-Nahid <mueem51@gmail.com>
2022-08-26 23:58:30 +0600

    formik, yup installed, working on login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 
 function App() {
    return (
-      <div className="App">
-         <h1>Welcome to React Router!</h1>
-         <Routes>
-            <Route path="/login" element={<Login />} exact />
-            <Route path="/profile" element={<Profile />} exact />
-            <Route path="/" element={<Home />} exact />
-         </Routes>
-      </div>
+      <Routes>
+         <Route path="/login" element={<Login />} exact />
+         <Route path="/profile" element={<Profile />} exact />
+         <Route path="/" element={<Home />} exact />
+      </Routes>
    );
 }
 
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import { Form, Formik } from 'formik'
+import { Link } from 'react-router-dom'
+
+import './style.css'
 
 export default function Login() {
    return (
-      <div>Login</div>
+      <div className='login'>
+         <div className='login_wrapper'>
+            <div className='login_wrap'>
+               <div className='login_1'>
+                  <img src='../../icons/facebook.svg' alt='facebook_logo' />
+                  <span>
+                     Facebook helps you to connect and share with the people in your life.
+                  </span>
+               </div>
+               <div className='login_2'>
+                  <div className='login_2_wrap'>
+                     <Formik>
+                        {
+                           (formik) => (
+                              <Form>
+                                 <input type='text' />
+                                 <input typpe='text' />
+                                 <button type='submit'>Log In</button>
+                              </Form>
+                           )
+                        }
+                     </Formik>
+                     <Link to='/forget-password'>Forget password?</Link>
+                     <div className='sign_splitter'></div>
+                     <button className='blue_btn open_signup'>Create Account</button>
+                  </div>
+                  <Link to='/'>
+                     <b>Create a Page</b> for celebraty, brand or business
+                  </Link>
+               </div>
+            </div>
+            <div className='register'></div>
+         </div>
+      </div>
    )
 }
