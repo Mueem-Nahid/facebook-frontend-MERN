dca6ba544efc78b374da51329433cbdf57b1ee9e
Mueem-Nahid <mueem51@gmail.com>
2022-10-21 22:41:01 +0600

    - register form added

---
+++ src/components/inputs/registerInput/index.js
@@ -0,0 +1,28 @@
+import { useMediaQuery } from 'react-responsive'
+import { ErrorMessage, useField } from 'formik'
+
+export default function RegisterInput({ placeholder, bottom, ...props }) {
+   const [field, meta] = useField(props);
+   const desktopView = useMediaQuery({
+      query: '(min-width: 850px)',
+   });
+
+   return (
+      <div className='input_wrap'>
+         {
+            meta.touched && meta.error && !bottom && <div className={desktopView ? 'input_error input_error_desktop' : 'input_error'} style={{ transform: 'translateY(4px)' }}>
+               <ErrorMessage name={field.name} />
+               <div className={desktopView ? 'error_arrow_left' : 'error_arrow_top'}></div>
+            </div>
+         }
+         <input className={meta.touched && meta.error ? 'input_error_border' : ''} type={field.type} name={field.name} placeholder={placeholder} {...field} {...props} />
+         {
+            meta.touched && meta.error && bottom && <div className={desktopView ? 'input_error input_error_desktop' : 'input_error'}>
+               <ErrorMessage name={field.name} />
+               <div className={desktopView ? 'error_arrow_left' : 'error_arrow_bottom'}></div>
+            </div>
+         }
+         {meta.touched && meta.error && <i className='error_icon' style={{ top: `${!bottom && !desktopView ? '63%' : '15px'}` }}></i>}
+      </div>
+   )
+}
--- src/components/login/RegisterForm.js
+++ src/components/login/RegisterForm.js
@@ -1,6 +1,88 @@
+import { Form, Formik } from "formik";
+import { useState } from "react";
+import RegisterInput from "../inputs/registerInput";
+
+const userInfos = {
+   firs_name: '',
+   last_name: '',
+   email: '',
+   password: '',
+   bYear: '',
+   bMonth: '',
+   bDay: '',
+   gender: ''
+}
 
 export default function RegisterForm() {
+   const [user, setUser] = useState(userInfos);
+
+   const handleRegisterChange = e => {
+      const { name, value } = e.target;
+      setUser({ ...user, [name]: value })
+   }
+
    return (
-      <div className="blur"></div>
+      <div className="blur">
+         <div className='register'>
+            <div className='register_header'>
+               <i className='exit_icon'></i>
+               <span>Sign up</span>
+               <span>It's quick and easy</span>
+            </div>
+            <Formik>
+               {
+                  (formik) => (
+                     <Form className="register_form">
+                        <div className="reg_line">
+                           <RegisterInput type='text' placeholder='First name' name='first_name' onChange={handleRegisterChange} />
+                           <RegisterInput type='text' placeholder='Last name' name='last_name' onChange={handleRegisterChange} />
+                        </div>
+                        <div className="reg_line">
+                           <RegisterInput type='text' placeholder='Mobile number or email address' name='email' onChange={handleRegisterChange} />
+                        </div>
+                        <div className="reg_line">
+                           <RegisterInput type='password' placeholder='New password' name='password' onChange={handleRegisterChange} />
+                        </div>
+                        <div className="reg_col">
+                           <div className="reg_line_header">
+                              Date of birth <i className="info_icon"></i>
+                           </div>
+                           <div className="reg_grid">
+                              <select name="bDay"><option>15</option></select>
+                              <select name="bMonth"><option>April</option></select>
+                              <select name="bYear"><option>1998</option></select>
+                           </div>
+                        </div>
+                        <div className="reg_col">
+                           <div className="reg_line_header">
+                              Gender <i className="info_icon"></i>
+                           </div>
+                           <div className="reg_grid">
+                              <label htmlFor="male">
+                                 Male
+                                 <input type='radio' name='gender' id='male' value='male' onChange={handleRegisterChange} />
+                              </label>
+                              <label htmlFor="female">
+                                 Female
+                                 <input type='radio' name='gender' id='female' value='female' onChange={handleRegisterChange} />
+                              </label>
+                              <label htmlFor="custom">
+                                 Custom
+                                 <input type='radio' name='gender' id='custom' value='custom' onChange={handleRegisterChange} />
+                              </label>
+                           </div>
+                        </div>
+                        <div className="reg_infos">
+                           By clicking Sign Up, you agree to our <span> Terms, Privacy Policy </span> and <span>Cookies Policy </span>. You may receive SMS notifications from us and can opt out at any time.
+                        </div>
+                        <div className="reg_btn_wrapper">
+                           <button className="blue_btn open_signup">Sign up</button>
+                        </div>
+                     </Form>
+                  )
+               }
+            </Formik>
+         </div>
+      </div>
    )
 }
