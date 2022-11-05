738f67d58c05b02902b02b3a15138effafdd5bfd
Mueem-Nahid <mueem51@gmail.com>
2022-11-05 22:43:54 +0600

    # Feature - custom hoot added - useClickOutside.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,20 @@
+import {useRef, useState} from "react";
 import Header from "../../components/header";
+import useClickOutside from "../../hooks/useClickOutside";
 
 export default function Home() {
+   const [visible, setVisible] = useState(true);
+   const el = useRef(null);
+
+   useClickOutside(el, () => {
+      setVisible(false);
+      // el.current.style.display = "none";
+   });
+
    return (
       <div>
          <Header/>
+         {visible && <div className="card" ref={el}></div>}
       </div>
    )
 }
