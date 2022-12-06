655fa127f5b74ed357a8f1d27530b9962c3fb769
Mueem-Nahid <mueem51@gmail.com>
2022-12-06 22:43:31 +0600

    # Feature - implement protected routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
+import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
 
 function App() {
    return (
       <Routes>
-         <Route path="/login" element={<Login />} exact />
-         <Route path="/profile" element={<Profile />} exact />
-         <Route path="/" element={<Home />} exact />
+         <Route element={<LoggedInRoutes/>}>
+            <Route path="/profile" element={<Profile/>} exact/>
+            <Route path="/" element={<Home/>} exact/>
+         </Route>
+         <Route element={<NotLoggedInRoutes/>}>
+            <Route path="/login" element={<Login/>} exact/>
+         </Route>
       </Routes>
    );
 }
---
+++ src/routes/LoggedInRoutes.js
@@ -0,0 +1,9 @@
+import {useSelector} from "react-redux";
+import {Outlet} from "react-router-dom";
+
+import Login from "../pages/login";
+
+export default function LoggedInRoutes() {
+   const {user} = useSelector((state) => ({...state}));
+   return user ? <Outlet/> : <Login/>
+}
