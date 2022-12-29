e7ad707b1b617a3ffb59085011b45e08bfb5fbfb
Mueem-Nahid <mueem51@gmail.com>
2022-12-30 00:54:50 +0600

    # Info - implement reset password form UI

--- src/pages/resetPassword/index.js
+++ src/pages/resetPassword/index.js
@@ -1,12 +1,17 @@
+import {Form, Formik} from "formik";
 import {Link} from "react-router-dom";
 import {useSelector} from "react-redux";
 
 import "./style.css";
 import useLogout from "../../hooks/useLogout";
+import {useState} from "react";
+import LoginInput from "../../components/inputs/loginInput";
 
 const ResetPassword = () => {
    const {user} = useSelector((state) => ({...state}));
    const logout = useLogout();
+   const [email, setEmail] = useState("");
+   const [error, setError] = useState("");
 
    return (
       <div className="reset">
@@ -26,6 +31,31 @@ const ResetPassword = () => {
                   </Link>
                )
             }
+         </div>
+         <div className="reset_wrap">
+            <div className="reset_form">
+               <div className="reset_form_header">Find your account</div>
+               <div className="reset_form_text">
+                  Please enter your email address or mobile number to search for your account.
+               </div>
+               <Formik enableReinitialize initialValues={{email,}}>
+                  {
+                     (formik) => (
+                        <Form>
+                           <LoginInput type="text" name="email" placeholder="Email or phone number"
+                                       onChange={(e) => setEmail(e.target.value)}/>
+                           {
+                              error && <div className="error_text">{error}</div>
+                           }
+                           <div className="reset_form_btns">
+                              <Link to="/login" className="gray_btn">Cancel</Link>
+                              <button type="submit" className="blue_btn">Search</button>
+                           </div>
+                        </Form>
+                     )
+                  }
+               </Formik>
+            </div>
          </div>
       </div>
    );
