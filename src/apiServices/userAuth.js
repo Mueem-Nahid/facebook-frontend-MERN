2166f7fbdfd66d56446634ab4299457e21f99d86
Mueem-Nahid <mueem51@gmail.com>
2023-03-08 20:29:37 +0600

    # Info - code refactored for sendVerification email

--- src/apiServices/userAuth.js
+++ src/apiServices/userAuth.js
@@ -9,3 +9,12 @@ export const registerUser = async ({first_name, last_name, email, password, bYea
    const response = await API.post('/register', {first_name, last_name, email, password, bYear, bMonth, bDay, gender});
    return response.data;
 }
+
+export const sendVerificationEmail = async (token) => {
+   const response = await API.post('/sendVerificationEmail', {}, {
+      headers: {
+         Authorization: `Bearer ${token}`,
+      },
+   });
+   return response.data;
+}
--- src/components/home/sendVerificationEmail/index.js
+++ src/components/home/sendVerificationEmail/index.js
@@ -1,21 +1,16 @@
-import './style.css';
 import {useState} from "react";
-import axios from "axios";
+
+import './style.css';
+import {sendVerificationEmail} from "../../../apiServices/userAuth";
 
 const SendVerificationEmail = ({user}) => {
    const [success, setSuccess] = useState("");
    const [error, setError] = useState("");
 
-   const sendVerificationEmail = async () => {
+   const handleSendVerificationEmail = async () => {
       try {
-         const {data} = await axios.post(
-            `${process.env.REACT_APP_BACKEND_URL}/sendVerificationEmail`, {},
-            {
-               headers: {
-                  Authorization: `Bearer ${user?.token}`,
-               },
-            }
-         );
+         const data = await sendVerificationEmail(user?.token);
+         console.log("sent:",data);
          setSuccess(data.message);
       } catch (error) {
          setError(error.response.data.message);
@@ -25,7 +20,8 @@ const SendVerificationEmail = ({user}) => {
    return (
       <div className="send_verification">
          <span>Your account is not verified. Verify your account before it gets deleted after a month from creating.</span>
-         <a className="verification_link" onClick={sendVerificationEmail}>Click here to resend verification link</a>
+         <a className="verification_link" onClick={handleSendVerificationEmail}>Click here to resend verification
+            link</a>
          {
             success && <div className="success_text">{success}</div>
          }
