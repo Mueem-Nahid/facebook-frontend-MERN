7f76b855accb6f31162031f2835d555374af29b6
Mueem-Nahid <mueem51@gmail.com>
2023-03-21 23:47:53 +0600

    # Info - image preview UI

--- src/components/createPostPopup/ImagePreview.js
+++ src/components/createPostPopup/ImagePreview.js
@@ -23,12 +23,42 @@ const ImagePreview = ({text, setText, images, setImages}) => {
    };
 
    return (
-      <div className="overflow_a">
+      <div className="overflow_a scrollbar">
          <TextareaWithEmojiPicker text={text} setText={setText} type2/>
          <div className="add_pictures_wrap">
             <input type="file" multiple hidden ref={imageInputRef} onChange={handleImages}/>
             {
-               images && images.length ? "" :
+               images && images.length ?
+                  <div className="add_pics_inside_1 p_0">
+                     <div className="preview_actions">
+                        <button className="hover1">
+                           <i className="edit_icon"></i>
+                           <span>Edit</span>
+                        </button>
+                        <button className="hover1">
+                           <i className="addPhoto_icon"></i>
+                           <span>Add</span>
+                        </button>
+                     </div>
+                     <div className="small_white_circle">
+                        <i className="exit_icon"></i>
+                     </div>
+                     <div className={
+                        images.length === 1 ? "preview_1" :
+                           images.length === 2 ? "preview_2" :
+                              images.length === 3 ? "preview_3" :
+                                 images.length === 4 ? "preview_4" :
+                                    images.length === 5 ? "preview_5" :
+                                       "preview_6"
+                     }>
+                        {
+                           images.map((img, i) => (
+                              <img src={img} key={i} alt={`image_${i}`}/>
+                           ))
+                        }
+                     </div>
+                  </div>
+                  :
                   <div className="add_pics_inside_1">
                      <div className="small_white_circle">
                         <i className="exit_icon"></i>
