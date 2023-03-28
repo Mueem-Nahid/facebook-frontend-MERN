cfa03349d496f9bc6b20c7a5f09e69a59ff253ad
Mueem-Nahid <mueem51@gmail.com>
2023-03-28 22:49:46 +0600

    # Info - implement single post UI

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -37,6 +37,37 @@ const Post = ({post}) => {
                <Dots color={dotsIconColor}/>
             </div>
          </div>
+         {
+            post?.background ?
+               <div className="post_bg" style={{backgroundImage: `url(${post?.background})`}}>
+                  <div className="post_bg_text">{post?.text}</div>
+               </div> :
+               <>
+                  <div className="post_text">
+                     {post?.text}
+                  </div>
+                  {
+                     post.images && post.images.length &&
+                     <div className={
+                        post.images.length === 1 ? "grid_1"
+                           : post.images.length === 2 ? "grid_2"
+                              : post.images.length === 3 ? "grid_3"
+                                 : post.images.length === 4 ? "grid_4"
+                                    : post.images.length >= 5 && "grid_5"
+                     }>
+                        {
+                           post.images.slice(0, 5).map((image, i) => (
+                              <img src={image.url} key={i} alt="" className={`img-${i}`}/>
+                           ))
+                        }
+                        {
+                           post.images.length > 5 && <div className="more-pics-shadow">+{post.images.length - 5}</div>
+                        }
+                     </div>
+                  }
+               </>
+
+         }
       </div>
    );
 };
