8380b07f6e04a93abe8141756f3f39cd44a69fdd
Mueem-Nahid <mueem51@gmail.com>
2023-04-20 23:20:16 +0600

    # Info - update profile picture post UI

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -54,30 +54,42 @@ const Post = ({post, user, profile}) => {
                <div className="post_bg" style={{backgroundImage: `url(${post?.background})`}}>
                   <div className="post_bg_text">{post?.text}</div>
                </div> :
-               <>
-                  <div className="post_text">
-                     {post?.text}
-                  </div>
-                  {
-                     post.images && post.images.length &&
-                     <div className={
-                        post.images.length === 1 ? "grid_1"
-                           : post.images.length === 2 ? "grid_2"
-                              : post.images.length === 3 ? "grid_3"
-                                 : post.images.length === 4 ? "grid_4"
-                                    : post.images.length >= 5 && "grid_5"
-                     }>
-                        {
-                           post.images.slice(0, 5).map((image, i) => (
-                              <img src={image.url} key={i} alt="" className={`img-${i}`}/>
-                           ))
-                        }
-                        {
-                           post.images.length > 5 && <div className="more-pics-shadow">+{post.images.length - 5}</div>
-                        }
+               post?.type === null ?
+                  <>
+                     <div className="post_text">
+                        {post?.text}
                      </div>
-                  }
-               </>
+                     {
+                        post.images && post.images.length &&
+                        <div className={
+                           post.images.length === 1 ? "grid_1"
+                              : post.images.length === 2 ? "grid_2"
+                                 : post.images.length === 3 ? "grid_3"
+                                    : post.images.length === 4 ? "grid_4"
+                                       : post.images.length >= 5 && "grid_5"
+                        }>
+                           {
+                              post.images.slice(0, 5).map((image, i) => (
+                                 <img src={image.url} key={i} alt="" className={`img-${i}`}/>
+                              ))
+                           }
+                           {
+                              post.images.length > 5 &&
+                              <div className="more-pics-shadow">+{post.images.length - 5}</div>
+                           }
+                        </div>
+                     }
+                  </> :
+                  post?.type === "profilePicture" ?
+                     <div className="post_profile_wrap">
+                        <div className="post_updated_bg">
+                           <img src={post?.user?.cover} alt=""/>
+                        </div>
+                        <img src={post?.images[0]?.url} className="post_updated_picture" alt=""/>
+                     </div> :
+                     <div className="post_cover_wrap">
+
+                     </div>
          }
          <div className="post_infos">
             <div className="reacts_count">
